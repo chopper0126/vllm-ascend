2678485d99daa916901315892e6cd523948b81c8
GuoRen868 <1269192170@qq.com>
2026-04-08 15:46:50 +0800

    add cam p2p ops.

--- csrc/build_aclnn.sh
+++ csrc/build_aclnn.sh
@@ -71,6 +71,8 @@ elif [[ "$SOC_VERSION" =~ ^ascend910_93 ]]; then
         "notify_dispatch"
         "moe_init_routing_custom"
         "moe_gating_top_k"
+        "a2e"
+        "e2a"
     )
     CUSTOM_OPS=$(IFS=';'; echo "${CUSTOM_OPS_ARRAY[*]}")
     SOC_ARG="ascend910_93"
