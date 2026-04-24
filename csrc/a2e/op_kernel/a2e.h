dd4b0a1ac216128deb05dc859fe5fa9e0a84789e
GuoRen868 <1269192170@qq.com>
2026-04-24 16:21:13 +0800

    cam_p2p: fix a2e recv_batch_size calculation error in asymmetric scenario.

--- csrc/a2e/op_kernel/a2e.h
+++ csrc/a2e/op_kernel/a2e.h
@@ -65,6 +65,7 @@ public:
 
         this->attnToMoeRatio = attentionRankSize > expertRankSize ? (attentionRankSize + expertRankSize - 1) /
             expertRankSize : 1;
+        recvBatchSize = batchSize / attnToMoeRatio;
         if (expertRankSize != 1 && attentionRankSize > expertRankSize && rank % expertRankSize >= attentionRankSize %
             expertRankSize && attentionRankSize % expertRankSize != 0) {
             this->attnToMoeRatio-=1;
@@ -128,10 +129,7 @@ public:
             } else {
                 int sendRank = rank + expertRankSize;
                 for (int i = 0; i < attnToMoeRatio; i++) {
-                    recvBatchSize = batchSize / attnToMoeRatio;
-
                     recvWithMte(i);
-
                     sendRank += expertRankSize;
                 }
             }
@@ -140,7 +138,6 @@ public:
                 int maskOutOffset = 0;
                 int sendRank = rank + expertRankSize;
                 for ( int i = 0; i < attnToMoeRatio; i++) {
-                    recvBatchSize = batchSize / attnToMoeRatio;
                     if (blockIdx < blockNum - 1) {
                         recvWithMte(i);
                     } else {
